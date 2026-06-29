96e1ce71f6a240583377f914d905cbb1f0c367b1
gayatribhise412-png <gayatribhise412@gmail.com>
2026-06-29 14:52:52 +0530

    Fix discount calculation and output formatting

--- task9.py
+++ task9.py
@@ -6,14 +6,13 @@ def calculate_discounted_price(price, discount_percent):
     discounted_price = price - discount_amount
     return discounted_price
 
-
 price = float(input("Enter original price: "))
 discount = float(input("Enter discount percentage: "))
 
-# Function call
+
 final_price = calculate_discounted_price(price, discount)
 
 
 print(f"Original Price: {price}")
-print(f"Discount {discount}%: {discount_amount:.2f}")
+print(f"Discount {discount}%: {(price * discount) / 100:.2f}")
 print(f"Final Price: {final_price:.2f}")
